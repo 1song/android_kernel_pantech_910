42f987e9fe03f7488e61d760bca89948a152d6c6
thanhphat11 <thanhphat1299@gmail.com>
2015-11-03 11:41:55 +0700

    edit fix pick error

--- include/linux/diagchar.h
+++ include/linux/diagchar.h
@@ -118,19 +118,11 @@ the appropriate macros. */
 
 /* This needs to be modified manually now, when we add
  a new RANGE of SSIDs to the msg_mask_tbl */
-<<<<<<< HEAD
-#define MSG_MASK_TBL_CNT		24
-#define EVENT_LAST_ID			0x09CB
-
-#define MSG_SSID_0			0
-#define MSG_SSID_0_LAST			97
-=======
 #define MSG_MASK_TBL_CNT		25
 #define EVENT_LAST_ID		0x0AA2
 
 #define MSG_SSID_0			0
 #define MSG_SSID_0_LAST			116
->>>>>>> adf1f8c... diag: Update SSIDs, event ids and log codes
 #define MSG_SSID_1			500
 #define MSG_SSID_1_LAST			506
 #define MSG_SSID_2			1000
@@ -162,11 +154,7 @@ the appropriate macros. */
 #define MSG_SSID_15			8000
 #define MSG_SSID_15_LAST		8000
 #define MSG_SSID_16			8500
-<<<<<<< HEAD
-#define MSG_SSID_16_LAST		8523
-=======
 #define MSG_SSID_16_LAST		8529
->>>>>>> adf1f8c... diag: Update SSIDs, event ids and log codes
 #define MSG_SSID_17			9000
 #define MSG_SSID_17_LAST		9008
 #define MSG_SSID_18			9500
@@ -178,17 +166,11 @@ the appropriate macros. */
 #define MSG_SSID_21			10300
 #define MSG_SSID_21_LAST		10300
 #define MSG_SSID_22			10350
-<<<<<<< HEAD
-#define MSG_SSID_22_LAST		10374
-#define MSG_SSID_23			0xC000
-#define MSG_SSID_23_LAST		0xC063
-=======
 #define MSG_SSID_22_LAST		10377
 #define MSG_SSID_23			10400
 #define MSG_SSID_23_LAST		10415
 #define MSG_SSID_24			0xC000
 #define MSG_SSID_24_LAST		0xC063
->>>>>>> adf1f8c... diag: Update SSIDs, event ids and log codes
 
 struct diagpkt_delay_params {
 	void *rsp_ptr;
@@ -311,8 +293,6 @@ static const uint32_t msg_bld_masks_0[] = {
 	MSG_LVL_LOW,
 	MSG_LVL_LOW|MSG_LVL_MED|MSG_LVL_HIGH|MSG_LVL_ERROR|MSG_LVL_FATAL,
 	MSG_LVL_MED,
-<<<<<<< HEAD
-=======
 	MSG_LVL_HIGH,
 	MSG_LVL_LOW,
 	MSG_LVL_HIGH,
@@ -335,7 +315,6 @@ static const uint32_t msg_bld_masks_0[] = {
 	MSG_LVL_LOW|MSG_LVL_MED|MSG_LVL_HIGH|MSG_LVL_ERROR|MSG_LVL_FATAL,
 	MSG_LVL_LOW|MSG_LVL_MED|MSG_LVL_HIGH|MSG_LVL_ERROR|MSG_LVL_FATAL,
 	MSG_LVL_MED
->>>>>>> adf1f8c... diag: Update SSIDs, event ids and log codes
 };
 
 static const uint32_t msg_bld_masks_1[] = {
@@ -677,15 +656,12 @@ static const uint32_t msg_bld_masks_16[] = {
 	MSG_LVL_LOW,
 	MSG_LVL_LOW,
 	MSG_LVL_LOW,
-<<<<<<< HEAD
-=======
 	MSG_LVL_LOW,
 	MSG_LVL_LOW|MSG_LVL_MED|MSG_LVL_HIGH|MSG_LVL_ERROR|MSG_LVL_FATAL,
 	MSG_LVL_LOW|MSG_LVL_MED|MSG_LVL_HIGH|MSG_LVL_ERROR|MSG_LVL_FATAL,
 	MSG_LVL_LOW|MSG_LVL_MED|MSG_LVL_HIGH|MSG_LVL_ERROR|MSG_LVL_FATAL,
 	MSG_LVL_LOW|MSG_LVL_MED|MSG_LVL_HIGH|MSG_LVL_ERROR|MSG_LVL_FATAL,
 	MSG_LVL_LOW|MSG_LVL_MED|MSG_LVL_HIGH|MSG_LVL_ERROR|MSG_LVL_FATAL
->>>>>>> adf1f8c... diag: Update SSIDs, event ids and log codes
 };
 
 static const uint32_t msg_bld_masks_17[] =  {
@@ -777,8 +753,6 @@ static const uint32_t msg_bld_masks_22[] = {
 	MSG_LVL_LOW,
 	MSG_LVL_LOW,
 	MSG_LVL_LOW,
-<<<<<<< HEAD
-=======
 	MSG_LVL_LOW,
 	MSG_LVL_LOW,
 	MSG_LVL_LOW,
@@ -801,7 +775,6 @@ static const uint32_t msg_bld_masks_23[] = {
 	MSG_LVL_LOW,
 	MSG_LVL_LOW,
 	MSG_LVL_LOW,
->>>>>>> adf1f8c... diag: Update SSIDs, event ids and log codes
 	MSG_LVL_LOW
 };
 
